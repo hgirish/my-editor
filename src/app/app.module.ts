f7aa5c052875e0e324ec5ddaaa38ff3abf26ad92
Girish <girish@post.com>
2021-10-09 21:13:28 -0700

    Configuring webpack for production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxWigModule } from 'ngx-wig';
 
@@ -12,6 +13,7 @@ import { EditorComponent } from './editor/editor.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgxWigModule,
   ],
   providers: [],
